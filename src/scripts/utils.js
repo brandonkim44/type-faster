73667849f991cc7d3da31bb82c072b3a8991d0ae
BrandonKim <brandonkim@virginia.edu>
2020-09-13 17:32:02 -0400

    Untracked dist folder

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -24,41 +24,45 @@ export function loadText(topic, selectedWPM) {
         .then(function (data) {
             const numOfArticles = data.articles.length;
             const randomArticleNum = Math.floor(Math.random() * numOfArticles + 1);
-            const article = data.articles[randomArticleNum];
-            const articleURL = article.url;
-        
-            const lexperAPIURL = `https://lexper.p.rapidapi.com/v1.1/extract?media=1&url=${articleURL}`;
-            fetch(
-                lexperAPIURL,
-                {
-                    method: "GET",
-                    headers: {
-                        "x-rapidapi-host": "lexper.p.rapidapi.com",
-                        "x-rapidapi-key":
-                        `${lexperAPI_key}`,
-                    },
-                })
-                .then(handleErrors)
-                .then((response) => {
-                    return response.json();
-                })
-                .then((data) => {
-                    console.log(data);
-                    const author = data.article.author;
-                    const title = data.article.title;
-                    const text = data.article.text;
-                    document.querySelector(".author").innerText = author;
-                    document.querySelector(".title").innerText = title;
-                    document.querySelector(".start-modal").style.display = "none";
-                    document.querySelector(".author-header").style.display = "flex";
-                    document.querySelector(".title-header").style.display = "flex";
-                    document.querySelector(".author").style.display = "flex";
-                    document.querySelector(".title").style.display = "flex";
-                    document.querySelector(".timer").style.display = "flex";
-                    document.querySelector(".menu").style.display = "flex";
-                    const game = new Game(text, selectedWPM);
-                    if (document.querySelector('.splash-bg')) { document.querySelector('.splash-bg').setAttribute('class', 'hidden-splash-bg'); }; 
-                })
+            if (article) {
+                const article = data.articles[randomArticleNum];
+                const articleURL = article.url;
+            
+                const lexperAPIURL = `https://lexper.p.rapidapi.com/v1.1/extract?media=1&url=${articleURL}`;
+                fetch(
+                    lexperAPIURL,
+                    {
+                        method: "GET",
+                        headers: {
+                            "x-rapidapi-host": "lexper.p.rapidapi.com",
+                            "x-rapidapi-key":
+                            `${lexperAPI_key}`,
+                        },
+                    })
+                    .then(handleErrors)
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then((data) => {
+                        console.log(data);
+                        const author = data.article.author;
+                        const title = data.article.title;
+                        const text = data.article.text;
+                        document.querySelector(".author").innerText = author;
+                        document.querySelector(".title").innerText = title;
+                        document.querySelector(".start-modal").style.display = "none";
+                        document.querySelector(".author-header").style.display = "flex";
+                        document.querySelector(".title-header").style.display = "flex";
+                        document.querySelector(".author").style.display = "flex";
+                        document.querySelector(".title").style.display = "flex";
+                        document.querySelector(".timer").style.display = "flex";
+                        document.querySelector(".menu").style.display = "flex";
+                        const game = new Game(text, selectedWPM);
+                        if (document.querySelector('.splash-bg')) { document.querySelector('.splash-bg').setAttribute('class', 'hidden-splash-bg'); }; 
+                    })
+            } else {
+                alert('Could not find an article with that word. Please try again!');
+            }
     });
 };
 
